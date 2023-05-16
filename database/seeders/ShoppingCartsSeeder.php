9c196a0827ae4d791d4bafa650e47f0b60424437
manu12l <95335354+manu12l@users.noreply.github.com>
2023-05-16 01:41:02 -0600

    shop

--- database/seeders/ShoppingCartsSeeder.php
+++ database/seeders/ShoppingCartsSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ShoppingCartsSeeder extends Seeder
 {
@@ -14,6 +15,61 @@ class ShoppingCartsSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $data = [
+           
+            [
+                'quantity' => 2,
+                'product_id' => 1,
+                'user_id' => 1,
+            ],
+            [
+                'quantity' => 1,
+                'product_id' => 2,
+                'user_id' => 1,
+            ],
+            [
+                'quantity' => 3,
+                'product_id' => 3,
+                'user_id' => 2,
+            ],
+            [
+                'quantity' => 2,
+                'product_id' => 4,
+                'user_id' => 2,
+            ],
+            [
+                'quantity' => 1,
+                'product_id' => 5,
+                'user_id' => 3,
+            ],
+
+            [
+                'quantity' => 2,
+                'product_id' => 6,
+                'user_id' => 1,
+            ],
+            [
+                'quantity' => 1,
+                'product_id' => 7,
+                'user_id' => 1,
+            ],
+            [
+                'quantity' => 3,
+                'product_id' => 8,
+                'user_id' => 2,
+            ],
+            [
+                'quantity' => 2,
+                'product_id' => 9,
+                'user_id' => 2,
+            ],
+            [
+                'quantity' => 1,
+                'product_id' => 10,
+                'user_id' => 3,
+            ],
+        ];
+
+        DB::table('shopping_carts')->insert($data);
     }
 }
